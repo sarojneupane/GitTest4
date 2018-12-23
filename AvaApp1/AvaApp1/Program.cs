5849e3d9815761774e12bd0fc464a8b9bd66a2e5
Saroj Neupane <neupanesaroj@gmail.com>
2018-12-23 18:16:32 -0500

    Created a project.

---
+++ AvaApp1/AvaApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test to see how github works");
+        }
+    }
+}
